2d484a58aa75902bef7f606aea5c28025a940479
Jacob Makil <makilj@sheridancollege.ca>
2021-02-09 15:01:39 -0500

    First Commit

--- src/averagegrade/AverageGrade.java
+++ src/averagegrade/AverageGrade.java
@@ -18,20 +18,25 @@ public class AverageGrade {
     grade[4] = 36;
     grade[5] = 93;
     grade[7] = 78;
-    grade[8] = 60;
-    grade[9] = 94;
+    grade[8] = 90;
+    grade[9] = 74;
        
     String error = "Cannot be Calculated!!!";
     //This code invoke the calcAvg() method to get the average grade and store it in averageGrade
     int averageGrade = calcAvg(grade);
-   
+    //This code invoke the calMax() method to get the highest grade in the array and store it into highestGrade
+    int highestGrade = calcMax(grade);
+ 
     //Using if statement to display the output of average.
     if (averageGrade == 101){
     System.out.println("There should be no negative grade");
     System.out.println("The average grade is: " + error);
     }else {
-      System.out.println("The Average Grade is: " + averageGrade + "%");
+    System.out.println("The Average Grade is: " + averageGrade + "%");
         }
+    //This code will display the highest grade in the Array
+    System.out.println("The Maximum Grade Is: " + highestGrade + "%");
+        
     }
   
     
@@ -52,6 +57,27 @@ public class AverageGrade {
     }
        return averageGrade;
     }
+    
+    // This method method figures out the maximum grade in the array and returns it. 
+    public static int calcMax(int[] y){
+    int highestGrade = 0;
+    int mark = 0;
+    for (int i = 0; i < y.length; i++){
+        if (y[i] > mark) {
+            mark = y[i];
+            highestGrade = mark;
+        }
+    }
+     return highestGrade;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 }
         
    
