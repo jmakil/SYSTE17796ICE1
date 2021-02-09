d6f86a6f8fb8438d644202aac26ed6c9d9a0c62b
jmakil <78267963+jmakil@users.noreply.github.com>
2021-02-09 14:48:31 -0500

    Update AverageGrade.java

--- src/averagegrade/AverageGrade.java
+++ src/averagegrade/AverageGrade.java
@@ -10,6 +10,7 @@ public class AverageGrade {
 
     public static void main(String[] args) {
   
+    //The grades being placed in an Array called grade
     int [] grade = new int[10];
     grade[0] = 56;
     grade[1] = 82;
@@ -21,8 +22,10 @@ public class AverageGrade {
     grade[9] = 94;
        
     String error = "Cannot be Calculated!!!";
+    //This code invoke the calcAvg() method to get the average grade and store it in averageGrade
     int averageGrade = calcAvg(grade);
    
+    //Using if statement to display the output of average.
     if (averageGrade == 101){
     System.out.println("There should be no negative grade");
     System.out.println("The average grade is: " + error);
@@ -32,6 +35,7 @@ public class AverageGrade {
     }
   
     
+    //Here is the calcAvg method. This method will calculate the average of all the grades in the array and return the average.
     public static int calcAvg(int[] y){
     int total = 0;
     int number = 0;
